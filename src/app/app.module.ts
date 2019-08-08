775cc3641f01c239ee50f5c73ff9d7f2ac086a49
francisco.arleo <francisco.arleo@globant.com>
2019-08-08 15:09:55 -0500

    Progress

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MatDatepickerModule, MatInputModule, MatFormFieldModule, MatButtonModule, MatIconModule, MatCardModule, MatOptionModule, MatSelectModule } from "@angular/material";
+import { SatDatepickerModule, SatNativeDateModule } from "saturn-datepicker";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+
+    // Material
+    MatDatepickerModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatIconModule,
+    MatCardModule,
+    MatOptionModule,
+    MatSelectModule,
+
+    // DatePicker Range
+    SatDatepickerModule,
+    SatNativeDateModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
